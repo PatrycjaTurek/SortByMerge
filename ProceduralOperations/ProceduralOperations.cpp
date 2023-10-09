a699585cc027773ce1efc49ceba19ad9c133679e
Patrycja Turek <patrycjaturek6@gmail.com>
2023-10-09 08:04:28 +0200

    removed some bugs. added simplified menu, upgraded display function

--- ProceduralOperations/ProceduralOperations.cpp
+++ ProceduralOperations/ProceduralOperations.cpp
@@ -4,6 +4,15 @@
 #include "stdfh.h"
 
 
+void display(std::vector<int>& drewNumbers)
+{
+	std::cout << "\n\n";
+	for (auto& y : drewNumbers)
+	{
+		std::cout << "\t" << y << "\n";
+	}
+	std::cout << "\n\n";
+}
 void drawNumbers(std::vector<int> &drewNumbers, int howMany) 
 {
 	if (drewNumbers.empty())
@@ -20,7 +29,8 @@ void drawNumbers(std::vector<int> &drewNumbers, int howMany)
 
 void sortMerge(std::vector<int> &drewNumbers, int beginingV, int endV) // re_merge if it's time to re-merge "subvectors"
 {
-	if(drewNumbers.size()<2) return;// shouldnt be a case but better add this than be sorry
+
+	//if(drewNumbers.size()<2) return;// shouldnt be a case but better add this than be sorry
 	if(drewNumbers.size() == 2)
 	{
 		if (drewNumbers[0] > drewNumbers[1])
@@ -32,10 +42,7 @@ void sortMerge(std::vector<int> &drewNumbers, int beginingV, int endV) // re_mer
 		return;
 	}
 	int halflenght = floor((endV) / 2);
-	if (endV - halflenght <=0)
-	{
-		return;
-	}
+
 	std::vector<int> VectorL (drewNumbers), VectorR;
 	VectorL.resize(halflenght);
 	std::copy(drewNumbers.begin()+ halflenght , drewNumbers.end(), std::back_inserter(VectorR));
@@ -43,77 +50,104 @@ void sortMerge(std::vector<int> &drewNumbers, int beginingV, int endV) // re_mer
 
 	if (VectorL.size() > 1)
 	{
-		sortMerge(VectorL, beginingV, VectorL.size()-1);
+		sortMerge(VectorL, beginingV, VectorL.size());
 	}
 	if (VectorR.size() > 1)
 	{
-		sortMerge(VectorR, beginingV, VectorR.size()-1);
+		sortMerge(VectorR, beginingV, VectorR.size());
 	}
 	std::vector <int> tmpVector;
 	tmpVector.clear();
-	int j = 0;
-	for (int i = 0; i < VectorL.size(); i++)
-	{
-		for (; j < VectorR.size(); j++)
+	int j = 0, i =0;
+	while (true)
+	{	
+		if (i >= VectorL.size())
 		{
-			if (VectorL[i] >= VectorR[j])
+			while (j<VectorR.size())
 			{
 				tmpVector.push_back(VectorR[j]);
+				j++;
 			}
-			else
+			break;
+		}
+		else if (j >= VectorR.size())
+		{
+			while (i < VectorL.size())
 			{
 				tmpVector.push_back(VectorL[i]);
-				break;
+				i++;
 			}
+			break;
 		}
-		if (j == VectorR.size())
+		else if(VectorL[i] < VectorR[j])
 		{
-			tmpVector.push_back(VectorL[i]); //if left vector is biger than right one
+			tmpVector.push_back(VectorL[i]);
+			i++;
+		}
+		else
+		{
+			tmpVector.push_back(VectorR[j]);
+			j++;
 		}
 
+
 	}
-	//VectorR.erase(drewNumbers.begin(),halflenght);
+
 	for (int i = 0; i < tmpVector.size(); i++)
 	{
 		drewNumbers[beginingV + i] = tmpVector[i];
 	}
 
-	int x;
 }
-
-void display(std::vector<int>& drewNumbers)
+bool isNOTIntegrer(std::string stringToCheck)
 {
-	for (auto& y : drewNumbers)
+	for (int i =0 ;i<stringToCheck.size();i++)
 	{
-		std::cout << y << "\n";
+		if (!isdigit(stringToCheck[i]))
+		{
+			return true;
+		}
 	}
+	return false;
 }
-int main()
+void takeUserNumbers(std::vector<int>& drewNumbers)
 {
-	srand(time(NULL));
-	std::vector<int> numbersVector;// = NULL;
-	
-	drawNumbers(numbersVector, 30);
-	display(numbersVector);
+	std::cout << "Enter number(one integrer). In case You don't want to gave another number enter anything but number\n";
+	std::string usernumber;
 
+	while (true)
+	{
+		std::cin >> usernumber;
+		if (isNOTIntegrer(usernumber))
+		{
+			break;
+		}
+		drewNumbers.push_back(atoi(usernumber.c_str()));
+	}
+
+	std::cout << "\tNumberers have been added\n\n";
+}
+
+void displayMenu(std::vector<int>& drewNumbers)
+{
 	char userAnswear;
 	bool exitLoop = true;
 	do
 	{
-		std::cout << "\n\n\tDo you want to add more numbers? \n\n\t 1 - randomize another 10 numbers\n\t 2 declare will to give your set of numbers\n\t 3 -exit\n\n\n";
+		std::cout << "\n\n\tDo you want to add more numbers? \n\n\t 1 - randomize another 10 numbers and start sorting\n\t 2 enter your set of numbers\n\t 3 -exit\n\n\n";
 		std::cin.get(userAnswear);
 		switch (userAnswear)
 		{
 		case '1':
 		{
-			drawNumbers(numbersVector, 10);
-			display(numbersVector);
+			drawNumbers(drewNumbers, 10);
+			display(drewNumbers);
 			break;
 		}
 		case '2':
 		{
-			//takeUserNumbers();
-			display(numbersVector);
+			takeUserNumbers(drewNumbers);
+			display(drewNumbers);
 			break;
 		}
 		case '3':
@@ -127,9 +161,19 @@ int main()
 			break;
 		}
 		}
+		std::cin.clear(); //to clear cin buffer in case user use key when operation was done;
 
+	} while (!exitLoop);
+}
+int main()
+{
+	srand(time(NULL));
+	std::vector<int> numbersVector;// = NULL;
+	
+	drawNumbers(numbersVector, 60);
+	display(numbersVector);
 
-	} while(!exitLoop);
+	displayMenu(numbersVector);
 
 	sortMerge(numbersVector, 0, numbersVector.size());
 	display(numbersVector);
